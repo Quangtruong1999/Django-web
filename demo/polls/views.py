f5da248fe1da7d9a1427f93c58659cb8c95fd759
Your Name <hideonbush0512@gmail.com>
2021-10-08 22:55:15 +0700

    update ver 0.1

--- demo/polls/views.py
+++ demo/polls/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+ 
+def index(request):
+    response = HttpResponse()
+    response.write("<h1>Welcome</h1>")
+    response.write("This is the polls aaaaaaaaaaaaaaaa app")
+    return response
